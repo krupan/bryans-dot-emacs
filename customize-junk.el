78d07673ddc5e93c32f5b95864371034ddce4e5c
Bryan Murdock <bmurdock@gmail.com>
2020-10-18 22:01:37 -0600

    fix some org-mode regressions, modes for yaml file editing

--- customize-junk.el
+++ customize-junk.el
@@ -9,17 +9,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-arguments (quote ("--word-regexp" "--smart-case")))
+ '(ag-arguments '("--word-regexp" "--smart-case"))
+ '(browse-url-browser-function 'browse-url-default-browser)
  '(custom-safe-themes
-   (quote
-    ("11e5e95bd3964c7eda94d141e85ad08776fbdac15c99094f14a0531f31a156da" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(dumb-jump-force-searcher (quote ag))
- '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
+   '("11e5e95bd3964c7eda94d141e85ad08776fbdac15c99094f14a0531f31a156da" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
+ '(dumb-jump-force-searcher 'ag)
+ '(magit-log-arguments '("--graph" "--color" "--decorate" "-n256"))
  '(org-agenda-files nil)
  '(package-selected-packages
-   (quote
-    (zig-mode htmlize django-mode dockerfile-mode go-mode hyperbole ztree forge ghub rust-auto-use adoc-mode slack cargo rust-mode hideshow-org clang-format memory-usage ido-completing-read+ cmake-mode lua-mode yaml-mode wgrep-ag smex smartrep smart-tab markdown-mode magit graphviz-dot-mode exec-path-from-shell crm-custom use-package)))
- '(send-mail-function (quote smtpmail-send-it))
+   '(polymode jinja2-mode indent-tools highlight-indent-guides dumb-jump zig-mode htmlize django-mode dockerfile-mode go-mode hyperbole ztree forge ghub rust-auto-use adoc-mode slack cargo rust-mode hideshow-org clang-format memory-usage ido-completing-read+ cmake-mode lua-mode yaml-mode wgrep-ag smex smartrep smart-tab markdown-mode magit graphviz-dot-mode exec-path-from-shell crm-custom use-package))
+ '(send-mail-function 'smtpmail-send-it)
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 587))
 (custom-set-faces
@@ -27,4 +26,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(cursor ((t (:background "white")))))
--- external-packages.el
+++ external-packages.el
@@ -23,6 +23,7 @@
 ;;(add-to-list 'package-archives (cons "melpa-stable" "https://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 27)
   (package-initialize))
+
 ;; Now, you have to install use-package with M-x package-install
 ;; before the following will work.  That's the one time you'll have to
 ;; manually run package-install (ideally, but I have had to manually
@@ -52,10 +53,7 @@
   :custom (dumb-jump-force-searcher 'ag)
   ;; replace xref:
   :config
-  (define-key global-map [remap xref-find-definitions]
-    'dumb-jump-go)
-  (define-key global-map [remap xref-pop-marker-stack]
-    'dumb-jump-back))
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
 (use-package color-theme-modern
   :ensure t)
@@ -76,6 +74,10 @@
 (use-package graphviz-dot-mode
   :ensure t)
 
+;; (use-package ido-completing-read+
+;;   :ensure t
+;;   :config (ido-ubiquitous-mode 1))
+
 (use-package magit
   :ensure t
   :custom (magit-completing-read-function 'magit-ido-completing-read))
@@ -95,20 +97,6 @@
   :ensure t
   :config (global-smart-tab-mode 1))
 
-(use-package smartrep
-  :ensure t
-  :config
-  ;; "Changing Window size tends to execute the same command
-  ;; continuously. It is boring work. So eval this example and type
-  ;; C-x { { {."
-  (smartrep-define-key
-   global-map "C-x"
-   '(("{" . shrink-window-horizontally)
-     ("}" . enlarge-window-horizontally)))
-  (smartrep-define-key
-      global-map "C-x"
-    '(("o" . other-window))))
-
 (use-package smex
   :ensure t
   ;; Invoke M-x without the Alt key.  I got this from
@@ -122,3 +110,96 @@
 
 (use-package yaml-mode
   :ensure t)
+
+;; more essentials for big hairy yaml file editing,
+;; highlight-indent-guides and indent-tools
+(use-package highlight-indent-guides
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+  (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode))
+;; I've avoided hydra for a while now, but indent-tools really wanted
+;; it, and after using magit for a while, it looks pretty normal to me
+;; now.  Oh, and it can replace smartrep!  OK, I'm sold.
+(use-package hydra
+  :ensure t
+  :config
+  (defhydra hydra-window-size (global-map "C-x")
+    "shrink and grow windows"
+    ("{" shrink-window-horizontally)
+    ("}" enlarge-window-horizontally))
+  (defhydra hydra-other-window (global-map "C-x")
+    "move to other window(s)"
+    ("o" other-window))
+  ;; from the hydra wiki:
+  ;; https://github.com/abo-abo/hydra/wiki/Rectangle-Operations
+  (defhydra hydra-rectangle (:body-pre (rectangle-mark-mode 1)
+                                       :color pink
+                                       :hint nil
+                                       :post (deactivate-mark))
+    "
+  ^_k_^       _w_ copy      _o_pen       _N_umber-lines            |\\     -,,,--,,_
+_h_   _l_     _y_ank        _t_ype       _e_xchange-point          /,`.-'`'   ..  \-;;,_
+  ^_j_^       _d_ kill      _c_lear      _r_eset-region-mark      |,4-  ) )_   .;.(  `'-'
+^^^^          _u_ndo        _q_ quit     ^ ^                      '---''(./..)-'(_\_)
+"
+    ("k" rectangle-previous-line)
+    ("j" rectangle-next-line)
+    ("h" rectangle-backward-char)
+    ("l" rectangle-forward-char)
+    ("d" kill-rectangle)                    ;; C-x r k
+    ("y" yank-rectangle)                    ;; C-x r y
+    ("w" copy-rectangle-as-kill)            ;; C-x r M-w
+    ("o" open-rectangle)                    ;; C-x r o
+    ("t" string-rectangle)                  ;; C-x r t
+    ("c" clear-rectangle)                   ;; C-x r c
+    ("e" rectangle-exchange-point-and-mark) ;; C-x C-x
+    ("N" rectangle-number-lines)            ;; C-x r N
+    ("r" (if (region-active-p)
+             (deactivate-mark)
+           (rectangle-mark-mode 1)))
+    ("u" undo nil)
+    ("q" nil))
+  (global-set-key (kbd "C-c R") 'hydra-rectangle/body))
+
+(use-package indent-tools
+  :ensure t
+  :config 
+  (global-set-key (kbd "C-c >") 'indent-tools-hydra/body)
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (define-key python-mode-map (kbd "C-c >")
+                'indent-tools-hydra/body)))
+  (add-hook 'yaml-mode-hook
+            (lambda ()
+              (define-key yaml-mode-map (kbd "C-c >")
+                'indent-tools-hydra/body))))
+
+;; needed for polymode below
+(use-package jinja2-mode
+  :ensure t)
+
+(use-package polymode
+  :ensure t
+  ;; pretty neat, a mode for jinja2 inside verilog.  I followed the
+  ;; example here:
+  ;; https://www.masteringemacs.org/article/polymode-multiple-major-modes-how-to-use-sql-python-in-one-buffer
+  :mode ("\\.jinja\\'" . poly-verilog-jinja2-mode)
+  :config
+  (define-hostmode poly-verilog-hostmode :mode 'verilog-mode)
+
+  ;; this function came: from https://gitlab.com/mavit/poly-ansible
+  (defcustom pm-inner/jinja2
+    (pm-inner-chunkmode :mode #'jinja2-mode
+                        :head-matcher "{[%{#][+-]?"
+                        :tail-matcher "[+-]?[%}#]}"
+                        :head-mode 'body
+                        :head-adjust-face nil)
+    "Jinja2 chunk."
+    :group 'innermodes
+    :type 'object)
+
+  (define-polymode poly-verilog-jinja2-mode
+    :hostmode 'poly-verilog-hostmode
+    :innermodes '(pm-inner/jinja2)
+    (verilog 1)))
