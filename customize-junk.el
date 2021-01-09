95e22d4b9cc29ca3dd8f53171359fb3072cfb2df
Bryan Murdock <bmurdock@gmail.com>
2021-01-09 04:38:17 -0700

    updated all packages, smart-tab works again

--- customize-junk.el
+++ customize-junk.el
@@ -9,11 +9,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ag-arguments '("--word-regexp" "--smart-case"))
+ '(ag-arguments '("--word-regexp" "--smart-case") nil nil "Customized with use-package ag")
  '(browse-url-browser-function 'browse-url-default-browser)
  '(custom-safe-themes
    '("11e5e95bd3964c7eda94d141e85ad08776fbdac15c99094f14a0531f31a156da" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
- '(dumb-jump-force-searcher 'ag)
+ '(dumb-jump-force-searcher 'ag nil nil "Customized with use-package dumb-jump")
  '(magit-log-arguments '("--graph" "--color" "--decorate" "-n256"))
  '(org-agenda-files nil)
  '(package-selected-packages
