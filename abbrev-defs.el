6667a2c1670ce643bec35917a8ed7203ed9ec736
Bryan Murdock <bmurdock@gmail.com>
2021-02-08 06:01:51 -0800

    don't need abbrev count properties

--- abbrev-defs.el
+++ abbrev-defs.el
@@ -1,31 +1,31 @@
 ;;-*-coding: utf-8;-*-
 (define-abbrev-table 'c-mode-abbrev-table
   '(
-    ("debug" "" skel-c-dbg-print :count 12)
-    ("debugh" "" skel-c-dbg-hexdump :count 9)
-    ("printf" "" skel-c-printf :count 2)
+    ("debug" "" skel-c-dbg-print)
+    ("debugh" "" skel-c-dbg-hexdump)
+    ("printf" "" skel-c-printf)
    ))
 
 (define-abbrev-table 'global-abbrev-table
   '(
-    ("adn" "and" nil :count 3)
-    ("dtt" "" skel-django-template-tag :count 21)
-    ("dtv" "" skel-django-template-var :count 7)
-    ("shr" "¯\\_(ツ)_/¯" nil :count 3)
-    ("teh" "the" nil :count 16)
-    ("uvmi" "" skel-verilog-insert-uvm-info :count 4)
-    ("uvmip" "" skel-verilog-insert-uvm-info-with-psprintf :count 2)
+    ("adn" "and" nil)
+    ("dtt" "" skel-django-template-tag)
+    ("dtv" "" skel-django-template-var)
+    ("shr" "¯\\_(ツ)_/¯" nil)
+    ("teh" "the" nil)
+    ("uvmi" "" skel-verilog-insert-uvm-info)
+    ("uvmip" "" skel-verilog-insert-uvm-info-with-psprintf)
    ))
 
 (define-abbrev-table 'org-mode-abbrev-table
   '(
-    ("mtodo" "[ ] " nil :count 4)
-    ("odh" "" skel-org-document-header :count 13)
-    ("onh" "" skel-org-notes-header :count 13)
+    ("mtodo" "[ ] " nil)
+    ("odh" "" skel-org-document-header)
+    ("onh" "" skel-org-notes-header)
    ))
 
 (define-abbrev-table 'python-mode-abbrev-table
   '(
-    ("bp" "import pdb; pdb.set_trace()" nil :count 4)
+    ("bp" "import pdb; pdb.set_trace()" nil)
    ))
 
